50911423ab651a89a1321fde277c54f5c7b1d67e
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-10-06 11:00:32 +0200

    v

--- src/component/Pages/Info/Info.jsx
+++ src/component/Pages/Info/Info.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./info.css";
 
 function Info() {
@@ -9,40 +9,45 @@ function Info() {
         <p className="description">
           please provide your name, email address, and phone number.
         </p>
+        <div className="input_group">
+          <div className="error_message">
+            <p>Name</p>
+            <span id="error_name"></span>
+          </div>
 
-        <div className="error_message">
-          <p>Name</p>
-          <span id="error_name"></span>
+          <input
+            type="text"
+            name=""
+            id="infoName"
+            placeholder="e.g. willson Mark"
+          />
         </div>
+        <div className="input_group">
+          <div className="error_message">
+            <p>Email Address</p>
+            <span id="error_email"></span>
+          </div>
 
-        <input
-          type="text"
-          name=""
-          id="infoName"
-          placeholder="e.g. willson Mark"
-        />
-        <div className="error_message">
-          <p>Email Address</p>
-          <span id="error_email"></span>
+          <input
+            type="text"
+            name=""
+            id="infoEmail"
+            placeholder="e.g. willsonMark@gmail.com"
+          />
         </div>
+        <div className="input_group">
+          <div className="error_message">
+            <p>Phone Number</p>
+            <span id="error_num"></span>
+          </div>
 
-        <input
-          type="text"
-          name=""
-          id="infoEmail"
-          placeholder="e.g. willsonMark@gmail.com"
-        />
-        <div className="error_message">
-          <p>Phone Number</p>
-          <span id="error_num"></span>
+          <input
+            type="text"
+            name=""
+            id="infoNumber"
+            placeholder="e.g. +47 234 567 78"
+          />
         </div>
-
-        <input
-          type="text"
-          name=""
-          id="infoNumber"
-          placeholder="e.g. +47 234 567 78"
-        />
 
         <div className="btn_container_step_one">
           <button>Next Step</button>
@@ -52,4 +57,4 @@ function Info() {
   );
 }
 
-export default Info
+export default Info;
